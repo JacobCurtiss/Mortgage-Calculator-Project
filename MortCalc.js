2b856eac5bb8153418c616086d4e4fa76c39d298
Jacob <jcurtiss94@outlook.com>
2019-08-09 13:41:43 -0400

    Javascript bug fixes and validation
    Moved all consts inside the mortgage function so the values would only be read after the user had input values
    
    Added validation to make sure a user input values without receiving an ugly NaN message

--- MortCalc.js
+++ MortCalc.js
@@ -18,58 +18,50 @@
 */
 
 //VARIABLE DECLARATIONS
-
-const homeValue = parseInt(document.getElementById("homevalueinput").value);
-
-const downPayment = parseInt(document.getElementById("downpaymentinput").value);
-
-const interestRate = parseInt(document.getElementById("interestrateinput").value);
-
-const loanLength = parseInt(document.getElementById("loanlengthinput").value);
-
-const loanStart = parseInt(document.getElementById("loandateinput").value);
-
-const monthlyHoa = parseInt(document.getElementById("hoainput").value);
-
-const monthlyInsurance = parseInt(document.getElementById("insuranceinput").value);
-
-const monthlyPropertyTax = parseInt(document.getElementById("propertytaxinput").value);
-
 const resultsBtn = document.getElementById("calculatebutton");
 
-const monthlyInterestRateBeforeRound = (interestRate / 12)
-console.log(monthlyInterestRateBeforeRound);
-
-const preDecimalMonthlyInterestRate = Math.round(100 * monthlyInterestRateBeforeRound) / 100;
-console.log(preDecimalMonthlyInterestRate);
-
-const monthlyInterestRate = preDecimalMonthlyInterestRate / 100;
-
-const monthlyPlusOne = monthlyInterestRate + 1;
-console.log(monthlyPlusOne);
-
-const loanLengthInMonths = loanLength * 12;
-console.log(loanLengthInMonths);
-
-const negPowInterestRate = Math.pow(monthlyPlusOne, (-1 * loanLengthInMonths));
-console.log(negPowInterestRate);
-
-const negPowSubtracted = 1 - negPowInterestRate;
-console.log(negPowSubtracted);
-
-const monthlyInterestDivided = monthlyInterestRate / negPowSubtracted;
-console.log(monthlyInterestDivided);
-
-const mortgageResult = (homeValue - downPayment) * monthlyInterestDivided;
-console.log(mortgageResult);
-
-const output = document.getElementById("output")
-//functionoutput
 function mortgage() {
-    
-    output.firstChild.data = `Your monthly mortgage payment is ${Math.round(mortgageResult)}`;
 
+    const homeValue = parseInt(document.getElementById("homevalueinput").value);
 
+    const downPayment = parseInt(document.getElementById("downpaymentinput").value);
+
+    const interestRate = parseInt(document.getElementById("interestrateinput").value);
+
+    const loanLength = parseInt(document.getElementById("loanlengthinput").value);
+
+    const loanStart = parseInt(document.getElementById("loandateinput").value);
+
+    const monthlyHoa = parseInt(document.getElementById("hoainput").value);
+
+    const monthlyInsurance = parseInt(document.getElementById("insuranceinput").value);
+
+    const monthlyPropertyTax = parseInt(document.getElementById("propertytaxinput").value);
+
+    const resultsBtn = document.getElementById("calculatebutton");
+
+    const monthlyInterestRateBeforeRound = (interestRate / 12)
+
+    const preDecimalMonthlyInterestRate = Math.round(100 * monthlyInterestRateBeforeRound) / 100;
+
+    const monthlyInterestRate = preDecimalMonthlyInterestRate / 100;
+
+    const monthlyPlusOne = monthlyInterestRate + 1;
+
+    const loanLengthInMonths = loanLength * 12;
+
+    const negPowInterestRate = Math.pow(monthlyPlusOne, (-1 * loanLengthInMonths));
+
+    const negPowSubtracted = 1 - negPowInterestRate;
+
+    const monthlyInterestDivided = monthlyInterestRate / negPowSubtracted;
+
+    const mortgageResult = (homeValue - downPayment) * monthlyInterestDivided;
+    console.log(mortgageResult);
+    const output = document.getElementById("output")
+    if (isNaN(mortgageResult)) {
+        return output.firstChild.data = "You have to enter values to receive a mortgage"
+    } else return output.firstChild.data = `Your monthly mortgage payment is ${Math.round(mortgageResult)}`;
 }
 
 resultsBtn.addEventListener("click", mortgage);
